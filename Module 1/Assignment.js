b5fbf6051a8e87f84cd39d4cff73fb8583fe1df6
abdullahabutasneem <40908199+abdullahabutasneem@users.noreply.github.com>
2023-03-23 21:41:21 +0600

    Create Assignment.js

---
+++ Module 1/Assignment.js
@@ -0,0 +1,11 @@
+<script>
+    function multiplicationTable(num) {
+        for (var i = 1; i <= 10; i++) {
+            var product = i * num;
+            console.log(num + " x " + i + " = " + product);
+        }
+    }
+    const input = 5;
+    multiplicationTable(input);
+
+</script>
