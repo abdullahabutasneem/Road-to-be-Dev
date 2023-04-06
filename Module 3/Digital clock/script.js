9fd9d54daa4e93fe27562dbb0651416e3760b1a6
abdullahabutasneem <40908199+abdullahabutasneem@users.noreply.github.com>
2023-04-06 23:11:03 +0600

    Script for digital clock

---
+++ Module 3/Digital clock/script.js
@@ -0,0 +1,37 @@
+const displayTime = () => {
+    // Declaring Date() methods and contain the data in dateTime variable
+    var dateTime = new Date();
+
+    // getting the hours, min, seconds by Date -> getHours(), getMinutes() and getSeconds() method.
+    // and saving the data in the hrs, min, sec variable
+    var hrs = dateTime.getHours().toString().padStart(2, '0');
+    var min = dateTime.getMinutes().toString().padStart(2, '0');
+    var sec = dateTime.getSeconds().toString().padStart(2, '0');
+
+    // we have to show the time is AM or PM. so we created an html span tag to show the AM/PM
+    // and get them by DOM manipulation
+
+    var meridiem;
+
+    // here is the condition for AM/PM. For greater than or equal to 12, we declare them as PM
+    // otherwise AM.
+    if (hrs >= 12) {
+        meridiem = 'PM'
+    }
+    else {
+        meridiem = 'AM'
+    }
+
+    // This conditon is for '12 hours' method.
+    // for example, it will show us 13:10 pm to 01:10 pm
+    if (hrs > 12) {
+        hrs = hrs - 12;
+    }
+
+    var time = `${hrs} : ${min} : ${sec} ${meridiem}`;
+    document.querySelector('.clock').textContent = time;
+}
+
+displayTime();
+
+setInterval(displayTime, 1000);
